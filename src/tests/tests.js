6cf5f9e62186876b646bb857bd0e250302042823
William Bowers <william.bowers@gmail.com>
2010-05-13 02:22:17 -0700

    Started porting the compression code. Added a license to the project (GPLv3).

--- src/tests/tests.js
+++ src/tests/tests.js
@@ -1,6 +1,14 @@
 JSTest.TestCase({
 	name: 'Basic Tests',
 	
+	testBadInputFails: function () {
+		this.assertRaises(zlib.Error, zlib.compress, null, null);
+		this.assertRaises(zlib.Error, zlib.compress, null, undefined);
+		this.assertRaises(zlib.Error, zlib.compress, null, 123);
+		this.assertRaises(zlib.Error, zlib.compress, null, {});
+		this.assertRaises(zlib.Error, zlib.compress, null, []);
+	},
+	
 	testCompressSmallBuffer: function () {
 		var input  = "hello";
 		var output = "\x78\x9c\xcb\x48\xcd\xc9\xc9\x07\x00\x06\x2c\x02\x15"
